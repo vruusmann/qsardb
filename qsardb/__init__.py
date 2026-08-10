955ba571ce8a9522984ec584c221bfdbc52476b9
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-08-10 13:04:36 +0300

    Extracted 'core' module

--- qsardb/__init__.py
+++ qsardb/__init__.py
@@ -1,7 +1,6 @@
 from pandas import DataFrame, Series
 from sklearn.pipeline import Pipeline
 from sklearn2pmml import make_pmml_pipeline, sklearn2pmml
-from xml.etree import ElementTree
 
 import os
 import pandas
@@ -9,83 +8,8 @@ import rdkit
 import shutil
 import sklearn
 import tempfile
-import zipfile
 
-NAMESPACE = "http://www.qsardb.org/QDB"
-
-ZIP_SUFFIXES = (".zip", ".qdb")
-
-CONTAINERS = {
-	"compounds" : ("CompoundRegistry", "Compound", ("Id", "Name", "Description", "Labels", "Cargos", "Cas", "InChI")),
-	"properties" : ("PropertyRegistry", "Property", ("Id", "Name", "Description", "Labels", "Cargos", "Endpoint", "Species")),
-	"descriptors" : ("DescriptorRegistry", "Descriptor", ("Id", "Name", "Description", "Labels", "Cargos", "Application")),
-	"models" : ("ModelRegistry", "Model", ("Id", "Name", "Description", "Labels", "Cargos", "PropertyId")),
-	"predictions" : ("PredictionRegistry", "Prediction", ("Id", "Name", "Description", "Labels", "Cargos", "ModelId", "Type", "Application"))
-}
-
-class QDB(object):
-
-	def __init__(self, name = None, description = None):
-		self.name = name
-		self.description = description
-		self.containers = {type : [] for type in CONTAINERS}
-		self.cargos = {type : {} for type in CONTAINERS}
-
-	def add(self, type, attributes, cargos):
-		attributes = dict(attributes)
-		attributes["Cargos"] = " ".join(cargos.keys())
-		self.containers[type].append(attributes)
-		self.cargos[type][attributes["Id"]] = cargos
-
-	def store(self, path):
-		if path.endswith(ZIP_SUFFIXES):
-			directory = tempfile.mkdtemp()
-			self._store(directory)
-			self._store_zip(directory, path)
-			shutil.rmtree(directory)
-		else:
-			self._store(path)
-		return path
-
-	def _store(self, directory):
-		if os.path.exists(directory):
-			shutil.rmtree(directory)
-		os.makedirs(directory)
-
-		self._store_xml(os.path.join(directory, "archive.xml"), "Archive", [{"Name" : self.name, "Description" : self.description}], ("Name", "Description"))
-
-		for type, (registry_tag, container_tag, order) in CONTAINERS.items():
-			if not self.containers[type]:
-				continue
-			self._store_xml(os.path.join(directory, type, type + ".xml"), registry_tag, self.containers[type], order, container_tag)
-			for id, cargos in self.cargos[type].items():
-				for cargo_id, payload in cargos.items():
-					self._store_cargo(directory, type, id, cargo_id, payload)
-
-	def _store_xml(self, path, registry_tag, containers, order, container_tag = None):
-		root = ElementTree.Element("{%s}%s" % (NAMESPACE, registry_tag))
-		for attributes in containers:
-			parent = root if container_tag is None else ElementTree.SubElement(root, "{%s}%s" % (NAMESPACE, container_tag))
-			for tag in order:
-				value = attributes.get(tag)
-				if value is not None:
-					ElementTree.SubElement(parent, "{%s}%s" % (NAMESPACE, tag)).text = str(value)
-		os.makedirs(os.path.dirname(path), exist_ok = True)
-		tree = ElementTree.ElementTree(root)
-		ElementTree.indent(tree)
-		tree.write(path, encoding = "UTF-8", xml_declaration = True, default_namespace = NAMESPACE)
-
-	def _store_cargo(self, directory, type, id, cargo_id, payload):
-		os.makedirs(os.path.join(directory, type, id), exist_ok = True)
-		with open(os.path.join(directory, type, id, cargo_id), "w", encoding = "UTF-8") as file:
-			file.write(payload)
-
-	def _store_zip(self, directory, path):
-		with zipfile.ZipFile(path, "w", zipfile.ZIP_DEFLATED) as archive:
-			for parent, _, names in os.walk(directory):
-				for name in names:
-					file_path = os.path.join(parent, name)
-					archive.write(file_path, os.path.relpath(file_path, directory))
+from qsardb.core import QDB, format_values
 
 class QsarDBPipeline(Pipeline):
 
@@ -207,8 +131,3 @@ def rdkit_application():
 def sklearn_application():
 	return "Scikit-Learn %s" % sklearn.__version__
 
-def format_values(id, values):
-	lines = ["Compound Id\t" + id]
-	for compound_id, value in values.items():
-		lines.append("%s\t%s" % (compound_id, round(float(value), 6)))
-	return "\n".join(lines)
