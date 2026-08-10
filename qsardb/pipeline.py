d39ece980f457398e0ddd87316ddf618352dea8d
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-08-10 13:06:15 +0300

    Extracted 'pipeline' module

---
+++ qsardb/pipeline.py
@@ -0,0 +1,133 @@
+from pandas import DataFrame, Series
+from sklearn.pipeline import Pipeline
+from sklearn2pmml import make_pmml_pipeline, sklearn2pmml
+
+import os
+import pandas
+import rdkit
+import shutil
+import sklearn
+import tempfile
+
+from qsardb.core import QDB, format_values
+
+class QsarDBPipeline(Pipeline):
+
+	def __init__(self, steps, memory = None, verbose = False):
+		super().__init__(steps, memory = memory, verbose = verbose)
+		self.set_output(transform = "pandas")
+		self.datasets = {}
+
+	def fit(self, X, y = None, **fit_params):
+		self._check(X, y)
+		descriptors = self._descriptor_steps().fit_transform(X[[X.columns[0]]])
+		descriptors.index = X.index
+		self.property_id = y.name
+		self.datasets = {}
+		self._estimator_steps().fit(descriptors, y)
+		self._record("training", X, y, descriptors)
+		return self
+
+	def validate(self, X, y = None):
+		self._check(X, y)
+		return self._record("validation", X, y, self._transform(X))
+
+	def test(self, X):
+		self._check(X, None)
+		return self._record("testing", X, None, self._transform(X))
+
+	def predict(self, X):
+		descriptors = self._transform(X)
+		return Series(self._estimator_steps().predict(descriptors), index = X.index, name = self.property_id)
+
+	def export(self, path, name = None, description = None):
+		if name is None:
+			name = self.property_id
+
+		qdb = QDB(name, description)
+
+		structures = self._merge("structures")
+		names = self._merge("names") if any(dataset["names"] is not None for dataset in self.datasets.values()) else None
+		for id, smiles in structures.items():
+			qdb.add("compounds", {"Id" : str(id), "Name" : None if names is None else names.get(id)}, {"daylight-smiles" : smiles})
+
+		property = self._merge("property")
+		qdb.add("properties", {"Id" : self.property_id, "Name" : self.property_id}, {"values" : format_values(self.property_id, property)})
+
+		descriptors = self._merge("descriptors")
+		for id in descriptors.columns:
+			qdb.add("descriptors", {"Id" : id, "Name" : id, "Application" : rdkit_application()}, {"values" : format_values(id, descriptors[id])})
+
+		qdb.add("models", {"Id" : "1", "Name" : name, "PropertyId" : self.property_id}, {"pmml" : self._format_pmml(descriptors.columns)})
+
+		for position, (type, dataset) in enumerate(self.datasets.items(), start = 1):
+			qdb.add("predictions", {"Id" : str(position), "Name" : type.capitalize() + " set", "ModelId" : "1", "Type" : type, "Application" : sklearn_application()}, {"values" : format_values(str(position), dataset["predictions"])})
+
+		return qdb.store(path)
+
+	def _check(self, X, y):
+		if not isinstance(X, DataFrame) or len(X.columns) < 1:
+			raise TypeError("X must be a DataFrame")
+		if y is not None:
+			if not isinstance(y, Series) or y.name is None:
+				raise TypeError("y must be a named Series")
+			if not X.index.equals(y.index):
+				raise ValueError("X and y must share the same compound identifiers")
+
+	def _record(self, type, X, y, descriptors):
+		predictions = Series(self._estimator_steps().predict(descriptors), index = X.index, name = self.property_id)
+		self.datasets[type] = {
+			"structures" : X[X.columns[0]],
+			"names" : self._select_names(X),
+			"property" : y,
+			"descriptors" : descriptors,
+			"predictions" : predictions
+		}
+		return predictions
+
+	def _transform(self, X):
+		descriptors = self._descriptor_steps().transform(X[[X.columns[0]]])
+		descriptors.index = X.index
+		return descriptors
+
+	def _select_names(self, X):
+		for column in X.columns[1:]:
+			if column.lower() == "name":
+				return X[column]
+		return None
+
+	def _descriptor_steps(self):
+		return Pipeline(self.steps[:self._boundary()])
+
+	def _estimator_steps(self):
+		return Pipeline(self.steps[self._boundary():])
+
+	def _boundary(self):
+		positions = [position for position, (_, step) in enumerate(self.steps) if type(step).__module__.startswith("scikit_mol")]
+		if not positions:
+			raise ValueError("The pipeline must begin with scikit-mol steps")
+		if positions != list(range(len(positions))):
+			raise ValueError("The scikit-mol steps must precede all other steps")
+		return len(positions)
+
+	def _merge(self, key):
+		parts = [dataset[key] for dataset in self.datasets.values() if dataset[key] is not None]
+		merged = pandas.concat(parts)
+		return merged[~merged.index.duplicated(keep = "last")].sort_index()
+
+	def _format_pmml(self, descriptor_ids):
+		pmml_pipeline = make_pmml_pipeline(self._estimator_steps(), active_fields = ["descriptors/" + id for id in descriptor_ids], target_fields = ["properties/" + self.property_id])
+		directory = tempfile.mkdtemp()
+		path = os.path.join(directory, "model.pmml")
+		sklearn2pmml(pmml_pipeline, path)
+		with open(path, "r", encoding = "UTF-8") as file:
+			pmml = file.read()
+		shutil.rmtree(directory)
+		return pmml
+
+def rdkit_application():
+	return "RDKit %s" % rdkit.__version__
+
+def sklearn_application():
+	return "Scikit-Learn %s" % sklearn.__version__
+
