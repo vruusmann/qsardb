50d1348b41764818731a82d3ad870d303009738f
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-08-10 22:10:26 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name = "qsardb",
 	version = "0.0.1",
-	description = "Creating QsarDB archives",
+	description = "QsarDB Python package",
 	packages = ["qsardb"],
-	install_requires = ["pandas", "scikit-learn", "scikit-mol", "sklearn2pmml"]
+	install_requires = ["numpy", "pandas", "rdkit", "scikit-learn", "scikit-mol", "sklearn2pmml"]
 )
